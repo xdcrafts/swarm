2a98b70c35eaad8d469ee159b250304878b5521d
vdubs <soranoo@mail.ru>
2015-09-23 10:51:09 +0300

    Usage example.

---
+++ src/main/java/org/swarm/async/ChannelUsage.java
@@ -0,0 +1,37 @@
+package org.swarm.async;
+
+import org.swarm.async.impl.Channel;
+import org.swarm.commons.FutureUtils;
+
+import java.time.Duration;
+
+import static org.swarm.commons.LangUtils.mapSupply;
+import static org.swarm.commons.LangUtils.supply;
+import static org.swarm.transducers.Implementations.map;
+
+/**
+ * ChannelUsage.
+ */
+public class ChannelUsage {
+
+    // todo move to tests
+
+    /**
+     * Main.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        final IChannel<String, Integer> channel = Channel
+            .<String, Integer>channel(map(mapSupply(Object::toString))).get();
+        channel.take().whenComplete((s, e) -> System.out.println(s + " - " + e));
+        channel.take().whenComplete((s, e) -> System.out.println(s + " - " + e));
+        channel.take().whenComplete((s, e) -> System.out.println(s + " - " + e));
+        channel.put(supply(1));
+        channel.put(supply(2)).whenComplete((s, e) -> System.out.println("put " + s + " - " + e));
+        FutureUtils.within(channel.put(supply(3)), Duration.ofMillis(100));
+        Thread.sleep(1000L);
+        channel.take().whenComplete((s, e) -> System.out.println(s + " - " + e));
+        channel.close();
+        channel.take().whenComplete((s, e) -> System.out.println(s + " - " + e));
+        Thread.sleep(1000L);
+    }
+}
